fd8524862948461e0a52bb305a6699941a3a622c
Gaspar Nagy <gaspar.nagy@gmail.com>
2010-09-06 14:14:59 +0200

    add file header

--- MiniDi/ObjectContainer.cs
+++ MiniDi/ObjectContainer.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/**************************************************************************************
+ * 
+ * MiniDi: A very simple IoC container, easily embeddable also as a source code. 
+ * 
+ * MiniDi was created to support SpecFlow (http://www.specflow.org) by Gaspar Nagy
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * 
+*/
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Diagnostics;
