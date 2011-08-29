f9bb3cdab435a8916cf2e1af6609b9b5865d60f7
Gaspar Nagy <gaspar.nagy@gmail.com>
2011-08-29 11:19:30 +0200

    fix build

---
+++ MiniDi.Tests/SubContainerTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MiniDi.Tests
+{
+    [TestFixture]
+    public class SubContainerTests
+    {
+    }
+}
