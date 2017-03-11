0fa8e2aee19cd7dd16ed129ad7ef71c0ca639dff
Rami Abughazaleh <icnocop@users.noreply.github.com>
2017-03-11 14:36:28 -0800

    Added IsRegistered methods to check if an interface or type is already registered Fixes #3

---
+++ BoDi.Tests/IsRegisteredTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+
+namespace BoDi.Tests
+{
+    [TestFixture]
+    public class IsRegisteredTests
+    {
+        [Test]
+        public void ShouldReturnFalseIfInterfaceNotRegistered()
+        {
+            // given
+
+            var container = new ObjectContainer();
+
+            // then
+
+            bool isRegistered = container.IsRegistered<IInterface1>();
+
+            Assert.IsFalse(isRegistered);
+        }
+
+        [Test]
+        public void ShouldReturnFalseIfTypeNotRegistered()
+        {
+            // given
+
+            var container = new ObjectContainer();
+
+            // then
+
+            bool isRegistered = container.IsRegistered<VerySimpleClass>();
+
+            Assert.IsFalse(isRegistered);
+        }
+
+        [Test]
+        public void ShouldReturnTrueIfInterfaceRegistered()
+        {
+            // given
+
+            var container = new ObjectContainer();
+
+            // when 
+
+            container.RegisterTypeAs<VerySimpleClass, IInterface1>();
+
+            // then
+
+            bool isRegistered = container.IsRegistered<IInterface1>();
+            
+            Assert.IsTrue(isRegistered);
+        }
+
+        [Test]
+        public void ShouldReturnTrueIfTypeRegistered()
+        {
+            // given
+
+            var container = new ObjectContainer();
+
+            // when 
+
+            container.RegisterInstanceAs(new SimpleClassWithDefaultCtor());
+
+            // then
+
+            bool isRegistered = container.IsRegistered<SimpleClassWithDefaultCtor>();
+
+            Assert.IsTrue(isRegistered);
+        }
+    }
+}
