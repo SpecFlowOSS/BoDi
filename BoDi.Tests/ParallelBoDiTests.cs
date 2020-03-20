b58523a506e84fc991b0912f0656a3de03a03bc4
Leonid Tsarev <leo@bastilia.ru>
2020-03-20 22:00:55 +0300

    Improve test

--- BoDi.Tests/ParallelBoDiTests.cs
+++ BoDi.Tests/ParallelBoDiTests.cs
@@ -20,16 +20,20 @@ namespace BoDi.Tests
             public IObjectContainer Create(IObjectContainer parent = null) => new ObjectContainer(parent);
         }
 
-        private static IEnumerable<IObjectContainer> Create(IBodiFactory factory)
+        private static IObjectContainer[] Create(IBodiFactory factory)
         {
             var parent = factory.Create();
-            parent.RegisterTypeAs<object, object>();
+            parent.RegisterTypeAs<object, object>().InstancePerContext();
 
-            var left = factory.Create(parent);
-            var right = factory.Create(parent);
+            IEnumerable<IObjectContainer> CreateChilds(int count)
+            {
+                for (var i = 1; i < count; i ++)
+                {
+                    yield return factory.Create(parent);
+                }
+            }
 
-            yield return left;
-            yield return right;
+            return CreateChilds(50).ToArray();
         }
 
         [Test]
@@ -38,17 +42,25 @@ namespace BoDi.Tests
             var containers = Create(new BoDiSimpleFactory());
 
             var result = await Task.WhenAll(containers.Select(c => Task.Run(() => c.Resolve<object>())).ToArray());
-            Assert.AreSame(result[0], result[1]);
+            AssertResultsAreSame(result);
         }
 
-       
+        private static void AssertResultsAreSame(object[] result)
+        {
+            var first = result.First();
+            foreach (var any in result)
+            {
+                Assert.AreSame(first, any);
+            }
+        }
+
 
         [Test]
         public void ShouldWorkWhenCreateSequentally()
         {
             var containers = Create(new BoDiSimpleFactory());
             var result = containers.Select(c => c.Resolve<object>()).ToArray();
-            Assert.AreSame(result[0], result[1]);
+            AssertResultsAreSame(result);
         }
     }
 }
