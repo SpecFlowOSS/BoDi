b357252999ba49c9e880e527a784081c2728adac
Leonid Tsarev <leo@bastilia.ru>
2020-03-20 19:20:42 +0300

    Add simple unit test to illustrate parallel problems

---
+++ BoDi.Tests/ParallelBoDiTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoDi.Tests
+{
+    [TestFixture]
+    public class ParallelBoDiTests
+    {
+        private interface IBodiFactory
+        {
+            IObjectContainer Create(IObjectContainer parent = null);
+
+        }
+        private class BoDiSimpleFactory : IBodiFactory
+        {
+            public IObjectContainer Create(IObjectContainer parent = null) => new ObjectContainer(parent);
+        }
+
+        private static IEnumerable<IObjectContainer> Create(IBodiFactory factory)
+        {
+            var parent = factory.Create();
+            parent.RegisterTypeAs<object, object>();
+
+            var left = factory.Create(parent);
+            var right = factory.Create(parent);
+
+            yield return left;
+            yield return right;
+        }
+
+        [Test]
+        public async Task ShouldWorkWhenCreateParallely()
+        {
+            var containers = Create(new BoDiSimpleFactory());
+
+            var result = await Task.WhenAll(containers.Select(c => Task.Run(() => c.Resolve<object>())).ToArray());
+            Assert.AreSame(result[0], result[1]);
+        }
+
+       
+
+        [Test]
+        public void ShouldWorkWhenCreateSequentally()
+        {
+            var containers = Create(new BoDiSimpleFactory());
+            var result = containers.Select(c => c.Resolve<object>()).ToArray();
+            Assert.AreSame(result[0], result[1]);
+        }
+    }
+}
