5f47cb41db8550dc9e4df824a5b27a5cdffec96a
bollhals <boli89@gmail.com>
2021-04-29 00:01:44 +0200

    add benchmarks

---
+++ BoDi.Performance.Tests/Benchmarks/RegisterTypeAs.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BODi.Performance.Tests.Benchmarks
+{
+    public class RegisterTypeAs : SingleEmptyContainerBenchmarkBase
+    {
+        [Benchmark(Baseline = true, Description = "v1.4")]
+        public void Version_1_4()
+        {
+            Container14.RegisterTypeAs<TypeRegistered, TypeRegistered>();
+        }
+
+        [Benchmark(Description = "v1.BoDi_Concurrent_Dictionary_And_Lazy")]
+        public void Version_1_BoDi_Concurrent_Dictionary_And_Lazy()
+        {
+            Container1Concurrent_Dictionary_And_Lazy.RegisterTypeAs<TypeRegistered, TypeRegistered>();
+        }
+
+        [Benchmark(Description = "Current")]
+        public void CurrentVersion()
+        {
+            ContainerCurrent.RegisterTypeAs<TypeRegistered, TypeRegistered>();
+        }
+    }
+}
---
+++ BoDi.Performance.Tests/Benchmarks/SingleEmptyContainerBenchmarkBase.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using BoDi;
+
+namespace BODi.Performance.Tests.Benchmarks
+{
+    [HtmlExporter]
+    [MarkdownExporterAttribute.GitHub]
+    [MemoryDiagnoser]
+    [MinColumn, MaxColumn, MeanColumn, MedianColumn, RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Declared)]
+    public abstract class SingleEmptyContainerBenchmarkBase
+    {
+        protected internal ObjectContainer ContainerCurrent;
+        protected internal BoDi1_4.ObjectContainer Container14;
+        protected internal BoDi_Concurrent_Dictionary_And_Lazy.ObjectContainer Container1Concurrent_Dictionary_And_Lazy;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            Container14 = new BoDi1_4.ObjectContainer();
+            Container1Concurrent_Dictionary_And_Lazy = new BoDi_Concurrent_Dictionary_And_Lazy.ObjectContainer();
+            ContainerCurrent = new ObjectContainer();
+        }
+
+        protected internal class FactoryRegistered { }
+
+        protected internal class TypeRegistered { }
+
+        protected internal interface IAllRegisteredFromType { }
+
+        protected internal interface IAllRegisteredFromFactory { }
+
+        private class AllRegistered1 : IAllRegisteredFromType, IAllRegisteredFromFactory {}
+        private class AllRegistered2 : IAllRegisteredFromType, IAllRegisteredFromFactory {}
+        private class AllRegistered3 : IAllRegisteredFromType, IAllRegisteredFromFactory {}
+        private class AllRegistered4 : IAllRegisteredFromType, IAllRegisteredFromFactory {}
+    }
+}
